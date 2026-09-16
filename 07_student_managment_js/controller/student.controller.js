02a8655584f3e6923fc44b3c648827b7cbffebb5
Gohil Gnandipainh <abhijitsinh438@gmail.com>
2026-09-16 14:28:11 +0530

    Refactor student controller for improved structure

--- 07_student_managment_js/controller/student.controller.js
+++ 07_student_managment_js/controller/student.controller.js
@@ -1,30 +1,103 @@
+import HttpError from "../middleware/HttpError.js";
 
+import Student from "../model/Student.js";
 
-import httpError from "../middleware/httpError.js";
-import Student from "../model/StudentData.js";
+const add = async (req, res, next) => {
+  try {
+    const { name, grId, email, course, isActive, mobileNumber } = req.body;
 
+    const newStudent = await new Student({
+      name,
+      grId,
+      email,
+      course,
+      isActive,
+      mobileNumber,
+    });
 
-const add = async (req,res,next)=>{
+    await newStudent.save();
 
-    try{
+    res.status(201).json({
+      success: true,
+      message: "student data added successfully",
+      newStudent,
+    });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
+const getAllStudentData = async (req, res, next) => {
+  try {
+    const students = await Student.find({});
 
-        const {name,email,GRID,Course,isActive} = req.body;
-
-        const newStudent = await new Student({
-     
-            name,email,Course,GRID,isActive
-
-        });
-
-        await newStudent.save();
-
-        res.status(201).json({success:true,message:"Student data added successfully",newStudent})
-    }catch(error){
-
-        next(new httpError(error.message,500));
-
+    if (students.length <= 0) {
+      res.status(200).json({ success: true, message: "no student data found" });
     }
 
-}
+    res.status(200).json({
+      success: true,
+      total: students.length,
+      message: "student data fetched successfully",
+      students,
+    });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
 
-export default add;
+const getStudentById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const student = await Student.findById(id);
+
+    if (!student) {
+      return next(new HttpError("student not found with this id", 404));
+    }
+
+    res.status(200).json({ success: true, message: "student found", student });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
+const deleteStudent = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const deleteStudent = await Student.findByIdAndDelete(id);
+
+    if (!deleteStudent) {
+      return next(new HttpError("student not deleted with this id", 400));
+    }
+
+    res
+      .status(200)
+      .json({ success: true, message: "student data deleted successfully" });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
+const deleteAllData = async (req, res, next) => {
+  try {
+    const deletedData = await Student.deleteMany();
+
+    if (!deletedData) {
+      return next(new HttpError("failed to delete data", 500));
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "all student data deleted successfully",
+    });
+  } catch (error) {
+    return next(new HttpError(error.message, 500));
+  }
+};
+
+export default {
+  add,
+  getAllStudentData,
+  getStudentById,
+  deleteStudent,
+  deleteAllData,
+};
